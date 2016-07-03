b7c7c03446c881a6e3d7efcdabd0bc022de144ee
Xuxue1 <1915998056@qq.com>
2016-07-03 23:01:30 +0800

    修改

--- src/main/scala/com/xuxue/code/scala/spider/IPTools.scala
+++ src/main/scala/com/xuxue/code/scala/spider/IPTools.scala
@@ -5,9 +5,10 @@ package com.xuxue.code.scala.spider
   */
 object IPTools {
 
-  def getIPRange(min:String,max:String):List={
+  def getIPRange(min:String,max:String):List[String]={
     val r=min.split(".")
     val a=r.map(_.toInt)
+    List("a")
   }
 
 }
